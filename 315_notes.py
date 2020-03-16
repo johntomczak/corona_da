d7719253d26e777b8d907f60c07ac857e8229d59
John Tomczak <johntomczak@Johns-MBP.attlocal.net>
2020-03-16 15:15:12 -0500

    okay

---
+++ 315_notes.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+Least 5 Losses
+City
+Tampa         -0.20
+Miami Beach   -0.23
+Honolulu      -0.32
+Minneapolis   -0.33
+Charlotte     -0.36
+
+Top 5 Losses
+City
+San Francisco   -0.72
+Boston          -0.70
+New York        -0.69
+Seattle         -0.62
+Los Angeles     -0.57
--- import_data.py
+++ import_data.py
@@ -1,22 +1,26 @@
 # coding: utf-8
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib as plt
 import sys
+import datetime
 from scipy.stats import norm
 
-
+d = datetime.datetime.now() - datetime.timedelta(days=1)
+d = d.strftime('%m_%d')
 a = 20
-fl = 'opentable_314.xlsx'
+fl = 'opentable_' + str(d) + '.xlsx'
 
 d0 = pd.read_excel(fl)
 d1 = d0.set_index('City')
-d1.columns = pd.Series(d1.columns).apply(lambda x: x.strftime('%m-%d') if type(x) != str else x)
+d1.columns = pd.Series(d1.columns).apply(lambda x: x.strftime('%m_%d') if type(x) != str else x)
 d1 = d1.drop('New Orleans')
-d1 = d1.drop(('AVERAGE'))
 d1 = d1.drop(('Ciudad de México'))
 d1 = d1.drop(('London'))
 d1 = d1.drop(('San Pedro Garza García'))
 d1 = d1.drop('Hamburg')
 d1 = d1.drop('München')
 d1 = d1.drop('Dublin')
+
+print( d1[d].sort_values(ascending=True).head(5) )
