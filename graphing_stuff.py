fa42e02fbd196c8b0fc4265c40cc4928b04aedd8
John Tomczak <johntomczak@Johns-MBP.attlocal.net>
2020-03-17 14:27:07 -0500

    just making notes

--- graphing_stuff.py
+++ graphing_stuff.py
@@ -4,7 +4,7 @@
 [sns.distplot(d1[x],hist=False,color=((y+0.5)/a,0.2,0.5,(y+0.5)/a), label=x, axlabel='YoY OpenTable Delta; Past '+str(a)+' Days' ) for y,x in enumerate(list(d1.columns)[-a:])]
 
 # a bar chart of just today, by city - it makes a pretty rainbow
-type(sns.barplot(d1[d].sort_values(), d1[d].sort_values().index ))
+sns.barplot(d1[d].sort_values(), d1[d].sort_values().index )
 
 # simply just show the timeline of a city of statistic
 sns.lineplot( d1.columns, d1.loc['Daily Median'] )
